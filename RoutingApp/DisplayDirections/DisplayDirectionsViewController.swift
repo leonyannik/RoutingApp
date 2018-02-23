980403dbb15dd3ca81e47de54954686c7a99cade
Leon Yannik <leonrlc@hotmail.com>
2018-02-23 17:23:41 -0600

    Connect the DisplayDirectionsViewController items to the swift File

--- RoutingApp/DisplayDirections/DisplayDirectionsViewController.swift
+++ RoutingApp/DisplayDirections/DisplayDirectionsViewController.swift
@@ -7,29 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
 class DisplayDirectionsViewController: UIViewController {
-
+    // MARK: - Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    
+    // MARK: - Constants
+    
+    // MARK: - Variables
+    
+    // MARK: - ViewController LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // MARK: - Outlet Actions
+    
+    // MARK: - Local Funtions
 }
